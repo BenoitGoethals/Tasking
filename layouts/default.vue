e934a882e219c53410908af63efa5b4449ee18c2
benoitgoethals <benoitgoethals@outlook.com>
2018-11-06 19:46:30 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item"
+
+            v-for="(item, key) of items"
+            :key="key">
+            <nuxt-link
+              :to="item.to"
+              exact-active-class="is-active">
+             {{ item.title }}
+            </nuxt-link>
+
+          </a>
+
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="main-content columns">
+
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">General</p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key">
+            <nuxt-link
+              :to="item.to"
+              exact-active-class="is-active">
+              <b-icon :icon="item.icon"/> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Home', icon: 'home', to: { name: 'index' } },
+        { title: 'Make new Task', icon: 'lightbulb', to: { name: 'InputTask' } },
+        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
+      ]
+    }
+  }
+}
+</script>
